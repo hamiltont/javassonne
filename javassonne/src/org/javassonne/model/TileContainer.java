67d40ee87bfa98b393e0abed6416ce7d86956d95
corespace <corespace@21451752-e034-11dd-bd7b-b12b6590754f>
2009-01-14 03:49:59 +0000

    Added TileContainerIterator interface and updated TileContainer interface.

--- javassonne/src/org/javassonne/model/TileContainer.java
+++ javassonne/src/org/javassonne/model/TileContainer.java
@@ -2,4 +2,14 @@ package org.javassonne.model;
 
 public interface TileContainer {
 
+	//Returns "home" square - the tile the game began with.
+	public Tile homeTile();
+	
+	//Adds tile to the specified TileContainerIterator location
+	public void addTile(TileContainerIterator iter, Tile tile);
+	
+	//Removes tile from the specified TileContainerIterator location
+	public Tile removeTile(TileContainerIterator iter);
+	
+	
 }
