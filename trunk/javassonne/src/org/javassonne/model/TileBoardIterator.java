af488b128f90b4967e7db595f33e3a2ae4dfbfbd
david.l.leinweber@vanderbilt.edu <david.l.leinweber@vanderbilt.edu@21451752-e034-11dd-bd7b-b12b6590754f>
2009-01-22 07:10:54 +0000

    - Made changes that Ben requested in Issue 5 (changed TileMapBoard, TileBoardIterator and IntPair)

--- trunk/javassonne/src/org/javassonne/model/TileBoardIterator.java
+++ trunk/javassonne/src/org/javassonne/model/TileBoardIterator.java
@@ -41,25 +41,25 @@ public class TileBoardIterator {
 
 	public TileBoardIterator down() {
 		previousLocation_ = location_;
-		location_ = new IntPair(location_.car() + 1, location_.cdr());
+		location_ = new IntPair(location_.first() + 1, location_.second());
 		return this;
 	}
 
 	public TileBoardIterator left() {
 		previousLocation_ = location_;
-		location_ = new IntPair(location_.car(), location_.cdr() - 1);
+		location_ = new IntPair(location_.first(), location_.second() - 1);
 		return this;
 	}
 
 	public TileBoardIterator right() {
 		previousLocation_ = location_;
-		location_ = new IntPair(location_.car(), location_.cdr() + 1);
+		location_ = new IntPair(location_.first(), location_.second() + 1);
 		return this;
 	}
 
 	public TileBoardIterator up() {
 		previousLocation_ = location_;
-		location_ = new IntPair(location_.car() - 1, location_.cdr());
+		location_ = new IntPair(location_.first() - 1, location_.second());
 		return this;
 	}
 
@@ -79,18 +79,18 @@ public class TileBoardIterator {
 	}
 
 	public boolean outOfBounds() {
-		if (location_.car() < data_.getUpperLeftCorner().location_.car()
-				|| location_.cdr() < data_.getUpperLeftCorner().location_.cdr()
-				|| location_.car() > data_.getUpperLeftCorner().location_.car()
-				|| location_.cdr() > data_.getUpperLeftCorner().location_.cdr())
+		if (location_.first() < data_.getUpperLeftCorner().location_.first()
+				|| location_.second() < data_.getUpperLeftCorner().location_.second()
+				|| location_.first() > data_.getUpperLeftCorner().location_.first()
+				|| location_.second() > data_.getUpperLeftCorner().location_.second())
 			return true;
 		return false;
 	}
 
 	// Advances iterator to start of next row
 	public TileBoardIterator nextRow() {
-		location_ = new IntPair(location_.car() + 1,
-				data_.getUpperLeftCorner().location_.cdr());
+		location_ = new IntPair(location_.first() + 1,
+				data_.getUpperLeftCorner().location_.second());
 		return this;
 	}
 
