e3f557addf9846b4c7e940663ff592c492cf05ae
corespace <corespace@21451752-e034-11dd-bd7b-b12b6590754f>
2009-01-25 22:56:13 +0000

    - Generated tests for TileBoardGenIterator and TileMapBoard in TileMapBoardTests - overrode equals(), toString(), and hashCode() in TileBoardGenIterator

--- javassonne/src/org/javassonne/model/TileBoardGenIterator.java
+++ javassonne/src/org/javassonne/model/TileBoardGenIterator.java
@@ -22,8 +22,10 @@ public class TileBoardGenIterator implements TileBoardIterator {
 
 	private TileBoard data_;
 	private IntPair location_;
-	//private IntPair previousLocation_;
 
+	// private IntPair previousLocation_;
+
+	
 	public TileBoardGenIterator(TileBoard data, IntPair intPair) {
 		data_ = data;
 		location_ = intPair;
@@ -32,10 +34,12 @@ public class TileBoardGenIterator implements TileBoardIterator {
 	public TileBoardGenIterator(TileBoardIterator old) {
 		data_ = old.getData();
 		location_ = old.getLocation();
-		//previousLocation_ = old.previousLocation_;
+		// previousLocation_ = old.previousLocation_;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.javassonne.model.TileBoardIterator#current()
 	 */
 	public Tile current() {
@@ -43,83 +47,89 @@ public class TileBoardGenIterator implements TileBoardIterator {
 	}
 
 	public TileBoardIterator down() {
-		//previousLocation_ = location_;
+		// previousLocation_ = location_;
 		location_ = new IntPair(location_.car() + 1, location_.cdr());
 		return this;
 	}
-	
+
 	public TileBoardIterator downCopy() {
-		return new TileBoardGenIterator(data_, new IntPair(location_.car() + 1, location_.cdr()));
+		return new TileBoardGenIterator(data_, new IntPair(location_.car() + 1,
+				location_.cdr()));
 	}
 
 	public TileBoardIterator left() {
-		//previousLocation_ = location_;
+		// previousLocation_ = location_;
 		location_ = new IntPair(location_.car(), location_.cdr() - 1);
 		return this;
 	}
-	
+
 	public TileBoardIterator leftCopy() {
-		return new TileBoardGenIterator(data_, new IntPair(location_.car(), location_.cdr() - 1));
+		return new TileBoardGenIterator(data_, new IntPair(location_.car(),
+				location_.cdr() - 1));
 	}
 
 	public TileBoardIterator right() {
-		//previousLocation_ = location_;
+		// previousLocation_ = location_;
 		location_ = new IntPair(location_.car(), location_.cdr() + 1);
 		return this;
 	}
-	
+
 	public TileBoardIterator rightCopy() {
-		return new TileBoardGenIterator(data_, new IntPair(location_.car(), location_.cdr() + 1));
+		return new TileBoardGenIterator(data_, new IntPair(location_.car(),
+				location_.cdr() + 1));
 	}
 
 	public TileBoardIterator up() {
-		//previousLocation_ = location_;
+		// previousLocation_ = location_;
 		location_ = new IntPair(location_.car() - 1, location_.cdr());
 		return this;
 	}
-	
+
 	public TileBoardIterator upCopy() {
-		return new TileBoardGenIterator(data_, new IntPair(location_.car() - 1, location_.cdr()));
+		return new TileBoardGenIterator(data_, new IntPair(location_.car() - 1,
+				location_.cdr()));
 	}
-/*
-	// Moves iterator to previous location (history size of 1)
-	public TileBoardIterator back() throws Exception {
-		if (previousLocation_ != null) {
-			location_ = previousLocation_;
-			previousLocation_ = null;
-			return this;
-		} else
-			throw new Exception();
-		// TODO: implement Exception
-	}
-*/
-	/* (non-Javadoc)
+
+	/*
+	 * // Moves iterator to previous location (history size of 1) public
+	 * TileBoardIterator back() throws Exception { if (previousLocation_ !=
+	 * null) { location_ = previousLocation_; previousLocation_ = null; return
+	 * this; } else throw new Exception(); // TODO: implement Exception }
+	 */
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.javassonne.model.TileBoardIterator#getLocation()
 	 */
 	public IntPair getLocation() {
 		return location_;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.javassonne.model.TileBoardIterator#outOfBounds()
 	 */
 	public boolean outOfBounds() {
 		if (location_.car() < data_.getUpperLeftCorner().getLocation().car()
-				|| location_.cdr() < data_.getUpperLeftCorner().getLocation().cdr()
-				|| location_.car() > data_.getUpperLeftCorner().getLocation().car()
-				|| location_.cdr() > data_.getUpperLeftCorner().getLocation().cdr())
+				|| location_.cdr() < data_.getUpperLeftCorner().getLocation()
+						.cdr()
+				|| location_.car() > data_.getUpperLeftCorner().getLocation()
+						.car()
+				|| location_.cdr() > data_.getUpperLeftCorner().getLocation()
+						.cdr())
 			return true;
 		return false;
 	}
 
 	// Advances iterator to start of next row
 	public TileBoardIterator nextRow() {
-//		previousLocation_ = location_;
-		location_ = new IntPair(location_.car() + 1,
-				data_.getUpperLeftCorner().getLocation().cdr());
+		// previousLocation_ = location_;
+		location_ = new IntPair(location_.car() + 1, data_.getUpperLeftCorner()
+				.getLocation().cdr());
 		return this;
 	}
-	
+
 	// Advances iterator to start of next row
 	public TileBoardIterator nextRowCopy() {
 		return new TileBoardGenIterator(data_, new IntPair(location_.car() + 1,
@@ -128,6 +138,25 @@ public class TileBoardGenIterator implements TileBoardIterator {
 
 	public TileBoard getData() {
 		return data_;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof TileBoardIterator))
+			return false;
+		else
+			return (this.data_.equals(((TileBoardIterator) obj).getData()) && 
+					this.location_.equals(((TileBoardIterator) obj).getLocation()));
+	}
+
+	@Override
+	public int hashCode() {
+		return this.toString().hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s:%s",data_.toString(),location_.toString());
 	}
 
 }
