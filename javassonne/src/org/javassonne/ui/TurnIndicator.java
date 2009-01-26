4aabdcc05d6df17cc67ee365ac12b0464374b7db
david.l.leinweber@vanderbilt.edu <david.l.leinweber@vanderbilt.edu@21451752-e034-11dd-bd7b-b12b6590754f>
2009-01-26 00:04:37 +0000

    - Added Turn Indicator, Rotate Buttons, and Draw Tile Button

---
+++ javassonne/src/org/javassonne/ui/TurnIndicator.java
@@ -0,0 +1,35 @@
+/**
+ * Javassonne 
+ *  http://code.google.com/p/javassonne/
+ * 
+ * @author David Leinweber
+ * @date Jan 25, 2009
+ * 
+ * Copyright 2009 Javassonne Team
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License. 
+ *  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software 
+ *  distributed under the License is distributed on an "AS IS" BASIS, 
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ *  implied. See the License for the specific language governing 
+ *  permissions and limitations under the License. 
+ */
+
+package org.javassonne.ui;
+
+import javax.swing.JLabel;
+
+public class TurnIndicator extends JLabel {
+	private int curPlayer_ = 0;
+	private static int MAX_SIZE = 6;
+
+	public TurnIndicator(String text) {
+		super(text);
+	}
+
+	public void nextPlayer() {
+		curPlayer_ = ((curPlayer_ + 1) % MAX_SIZE);
+	}
+
+}
