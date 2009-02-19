13b0944e4653ccf9a8a1fbe3123b0c2d017b47e9
bs.sals <bs.sals@21451752-e034-11dd-bd7b-b12b6590754f>
2009-02-19 07:49:43 +0000

    This is a prototype for the networking API.

--- javassonne/src/org/javassonne/networking/JavassonneHost.java
+++ javassonne/src/org/javassonne/networking/JavassonneHost.java
@@ -15,9 +15,46 @@
  *  implied. See the License for the specific language governing 
  *  permissions and limitations under the License. 
  */
-
 package org.javassonne.networking;
 
+import net.jxta.discovery.DiscoveryEvent;
+import net.jxta.discovery.DiscoveryListener;
+import net.jxta.discovery.DiscoveryService;
+import net.jxta.document.Advertisement;
+import net.jxta.document.AdvertisementFactory;
+import net.jxta.id.ID;
+import net.jxta.id.IDFactory;
+import net.jxta.peergroup.PeerGroup;
+import net.jxta.peergroup.PeerGroupID;
+import net.jxta.pipe.PipeService;
+import net.jxta.platform.NetworkManager;
+import net.jxta.protocol.DiscoveryResponseMsg;
+import net.jxta.protocol.PipeAdvertisement;
+import net.jxta.impl.util.BidirectionalPipeService;
+import net.jxta.impl.util.*;
+import java.io.File;
+import java.util.Enumeration;
+
+
 public class JavassonneHost {
+	public boolean activelyPlaying_;
+	public ID[] clientIds_; // the JXTA unique ids
+
+	public void setID(){}
+
+	public void setIP(String ip){}
+
+	public void connect(){
+	        NetworkManager manager = new NetworkManager(NetworkManager.ConfigMode.EDGE,
+	                        "JavassonneHost", new File(new File(".cache"),
+	                                "JavassonneHost").toURI());
+	          
+	        manager.startNetwork();
+	        PeerGroup defaultPeerGroup = manager.getNetPeerGroup();
+	        BidirectionalPipeService bps = new BidirectionalPipeService(defaultPeerGroup);
+	        BidirectionalPipeService.AcceptPipe acceptPipe = bps.bind("hpipe");
+	        PipeAdvertisement adv = acceptPipe.getAdvertisement();
+	        BidirectionalPipeService.Pipe hpipe = acceptPipe.accept(30 * 1000);
+	    }
 
 }
