44132dce9d4527226bd58f0498b6c941444654ac
david.l.leinweber@vanderbilt.edu <david.l.leinweber@vanderbilt.edu@21451752-e034-11dd-bd7b-b12b6590754f>
2009-03-22 23:35:02 +0000

    - Updated GameController and InputPlayerDataPanel so that the user can now select their MeepleColor from a drop down menu - Still need to write code to prevent users from selecting the same MeepleColors

--- trunk/javassonne/src/org/javassonne/ui/panels/InputPlayerDataPanel.java
+++ trunk/javassonne/src/org/javassonne/ui/panels/InputPlayerDataPanel.java
@@ -27,6 +27,7 @@ import java.util.List;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -34,25 +35,34 @@ import javax.swing.OverlayLayout;
 
 import org.javassonne.messaging.Notification;
 import org.javassonne.messaging.NotificationManager;
+import org.javassonne.model.Player;
+import org.javassonne.model.Player.MeepleColor;
 import org.javassonne.ui.DisplayHelper;
 import org.javassonne.ui.JKeyListener;
 
 public class InputPlayerDataPanel extends AbstractHUDPanel implements
 		ActionListener {
+	private static final String NEW_GAME_START_IMAGE = "images/new_game_start.jpg";
+	private static final String NEW_GAME_CANCEL_IMAGE = "images/new_game_cancel.jpg";
+	private static final String NEW_GAME_IMAGE = "images/new_game_background.jpg";
+
 	private JPanel generalButtons_;
 	private JPanel textFields_;
+	private JPanel labels_;
+	private JPanel comboBoxes_;
 
 	public InputPlayerDataPanel() {
 		super();
 
 		addKeyListener(JKeyListener.getInstance());
-		//TODO: Externalize string
-		setBackgroundImagePath("images/new_game_background.jpg");
+		// TODO: Externalize string
+		setBackgroundImagePath(NEW_GAME_IMAGE);
 		setVisible(true);
 		setSize(800, 600);
 		setLayout(new OverlayLayout(this));
 		setAlignmentY(CENTER_ALIGNMENT);
 
+		// TODO: Use BoxLayout instead of null layout
 		generalButtons_ = new JPanel();
 		generalButtons_.setOpaque(false);
 		generalButtons_.setLayout(null);
@@ -63,14 +73,32 @@ public class InputPlayerDataPanel extends AbstractHUDPanel implements
 		textFields_.setLayout(null);
 		textFields_.setAlignmentY(CENTER_ALIGNMENT);
 
-		//TODO: Use Java default layout organizers to eliminate ugly integers
+		labels_ = new JPanel();
+		labels_.setOpaque(false);
+		labels_.setLayout(null);
+		labels_.setAlignmentY(CENTER_ALIGNMENT);
+		
+		comboBoxes_ = new JPanel();
+		comboBoxes_.setOpaque(false);
+		comboBoxes_.setLayout(null);
+		comboBoxes_.setAlignmentY(CENTER_ALIGNMENT);
+
+		// TODO: Use Java default layout organizers to eliminate ugly integers
 		// add the buttons to the generalButtons_ panel
-		addButtonToPanel("images/new_game_cancel.jpg", "", 
-				new Point(25, 543), generalButtons_);
-		addButtonToPanel("images/new_game_start.jpg", Notification.START_GAME, 
+		addButtonToPanel(NEW_GAME_CANCEL_IMAGE, "", new Point(25, 543),
+				generalButtons_);
+		addButtonToPanel(NEW_GAME_START_IMAGE, Notification.START_GAME,
 				new Point(492, 543), generalButtons_);
 
-		// add the text boxes to the textFields_ panel
+		// add the labels to labels_ panel
+		addLabelToPanel(new Point(200, 150), "Player 1", labels_);
+		addLabelToPanel(new Point(200, 195), "Player 2", labels_);
+		addLabelToPanel(new Point(200, 240), "Player 3", labels_);
+		addLabelToPanel(new Point(200, 285), "Player 4", labels_);
+		addLabelToPanel(new Point(200, 330), "Player 5", labels_);
+		addLabelToPanel(new Point(200, 375), "Player 6", labels_);
+
+		// add text boxes to the textFields_ panel
 		addTextBoxToPanel(new Point(250, 150), textFields_);
 		addTextBoxToPanel(new Point(250, 195), textFields_);
 		addTextBoxToPanel(new Point(250, 240), textFields_);
@@ -78,9 +106,27 @@ public class InputPlayerDataPanel extends AbstractHUDPanel implements
 		addTextBoxToPanel(new Point(250, 330), textFields_);
 		addTextBoxToPanel(new Point(250, 375), textFields_);
 
-		// add panels 
+		// place MeepleColors into ImageIcon array
+		ImageIcon[] colors = new ImageIcon[MeepleColor.values().length];
+		int i = 0;
+		for (MeepleColor color : MeepleColor.values()) {
+			colors[i] = color.image;
+			i++;
+		}
+
+		// add combo boxes to comboBoxes_ panel
+		addComboBoxToPanel(new Point(550, 150), colors, 0, comboBoxes_);
+		addComboBoxToPanel(new Point(550, 195), colors, 1, comboBoxes_);
+		addComboBoxToPanel(new Point(550, 240), colors, 2, comboBoxes_);
+		addComboBoxToPanel(new Point(550, 285), colors, 3, comboBoxes_);
+		addComboBoxToPanel(new Point(550, 330), colors, 4, comboBoxes_);
+		addComboBoxToPanel(new Point(550, 375), colors, 5, comboBoxes_);
+
+		// add panels
 		add(textFields_);
 		add(generalButtons_);
+		add(labels_);
+		add(comboBoxes_);
 	}
 
 	private void addButtonToPanel(String imagePath, String notification,
@@ -89,28 +135,36 @@ public class InputPlayerDataPanel extends AbstractHUDPanel implements
 		b.addActionListener(this);
 		b.setActionCommand(notification);
 		b.setLocation(location);
-		//TODO: explain this weird size?
+		// TODO: explain this weird size?
 		b.setSize(279, 38);
 		panel.add(b);
 	}
 
 	private void addTextBoxToPanel(Point location, JPanel panel) {
 		JTextField text = new JTextField();
-		// text.addActionListener(this);
-		// text.setActionCommand(notification);
 		text.setLocation(location);
 		text.setSize(279, 38);
 		panel.add(text);
 	}
-	
-	private void addLabelToPanel(Point location, String text, JPanel panel)
-	{
+
+	private void addLabelToPanel(Point location, String text, JPanel panel) {
 		JLabel label = new JLabel(text);
 		label.setLocation(location);
+		label.setSize(50, 38);
 		panel.add(label);
 	}
 
-	public List<String> getPlayerData() {
+	private void addComboBoxToPanel(Point location, ImageIcon[] colors, int selectedIndex,
+			JPanel panel) {
+		JComboBox comboBox = new JComboBox(colors);
+		comboBox.addActionListener(this);
+		comboBox.setLocation(location);
+		comboBox.setSize(50, 38);
+		comboBox.setSelectedIndex(selectedIndex);
+		panel.add(comboBox);
+	}
+
+	public List<String> getPlayerNames() {
 		List<String> list = new ArrayList<String>();
 
 		// for each Component/JTextField in textFields_ add the text in the text
@@ -118,7 +172,19 @@ public class InputPlayerDataPanel extends AbstractHUDPanel implements
 		for (Component c : textFields_.getComponents()) {
 			list.add(((JTextField) c).getText());
 		}
-		//TODO: Does this return empty text too?
+		// TODO: Does this return empty text too?
+		return list;
+	}
+
+	public List<MeepleColor> getPlayerColors()
+	{
+		List<MeepleColor> list = new ArrayList<MeepleColor>();
+		
+		// for each Component/JComboBox in comboBoxes add the color selected to the list
+		for (Component c : comboBoxes_.getComponents()) {
+			list.add(MeepleColor.values()[((JComboBox) c).getSelectedIndex()]);
+		}
+		
 		return list;
 	}
 
@@ -134,7 +200,7 @@ public class InputPlayerDataPanel extends AbstractHUDPanel implements
 		if (e.getActionCommand().length() > 0)
 			NotificationManager.getInstance().sendNotification(
 					e.getActionCommand(), this);
-		else{
+		else {
 			// user pressed cancel
 			DisplayHelper.getInstance().remove(this);
 			NotificationManager.getInstance().removeObserver(this);
