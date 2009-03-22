ae0fa9195ec556273508bb4d1738e4de601cfdd9
adammalbright <adammalbright@21451752-e034-11dd-bd7b-b12b6590754f>
2009-03-22 22:23:29 +0000

    - Fixed issue of game window minimizing when JOptionPane class is used for user interaction

--- javassonne/src/org/javassonne/ui/DisplayWindow.java
+++ javassonne/src/org/javassonne/ui/DisplayWindow.java
@@ -23,6 +23,8 @@ package org.javassonne.ui;
 
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 import javax.swing.JFrame;
 
@@ -32,15 +34,15 @@ import org.javassonne.ui.controls.DisplayDesktopPane;
  * @author Hamilton Turner
  * 
  *         The Display serves only the purpose of wrapper-ing the
- *         LayeredDisplayPane so that it can be set to full-screen mode.
- *         It takes the responsibility of creating the layeredDisplayPane, 
- *         which creates both the map and the mapScrollEdges. 
+ *         LayeredDisplayPane so that it can be set to full-screen mode. It
+ *         takes the responsibility of creating the layeredDisplayPane, which
+ *         creates both the map and the mapScrollEdges.
  */
-public class DisplayWindow extends JFrame {
+public class DisplayWindow extends JFrame implements WindowListener {
 
 	private static final String WINDOW_TITLE = "Javassonne";
 	private static boolean FULL_SCREEN = true;
-	
+
 	private DisplayDesktopPane displayPane_;
 
 	/**
@@ -49,52 +51,92 @@ public class DisplayWindow extends JFrame {
 	 */
 	public DisplayWindow() {
 		super();
-		
-		GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+
+		GraphicsDevice device = GraphicsEnvironment
+				.getLocalGraphicsEnvironment().getDefaultScreenDevice();
 		boolean fakeFullscreen = false;
-		
+
 		// determine whether we need to fake full screen.
-		 try{
-			   String os = System.getProperty("os.name");
-			   if (os.equals("Mac OS X"))
-				   fakeFullscreen = true;
-			   
-		 }catch (Exception e) {
-			 // who cares?
-		 }
-		
+		try {
+			String os = System.getProperty("os.name");
+			if (os.equals("Mac OS X"))
+				fakeFullscreen = true;
+
+		} catch (Exception e) {
+			// who cares?
+		}
+
 		// Set a few basic properties of our window
 		setTitle(WINDOW_TITLE);
-		
-		if (FULL_SCREEN){
-			if (fakeFullscreen){
-				setSize(device.getDisplayMode().getWidth(), device.getDisplayMode().getHeight()-20);
-			}else
-				setSize(device.getDisplayMode().getWidth(), device.getDisplayMode().getHeight());
-			
+
+		if (FULL_SCREEN) {
+			if (fakeFullscreen) {
+				setSize(device.getDisplayMode().getWidth(), device
+						.getDisplayMode().getHeight() - 20);
+			} else
+				setSize(device.getDisplayMode().getWidth(), device
+						.getDisplayMode().getHeight());
+
 		} else {
 			setSize(1024, 768);
-			setLocation((device.getDisplayMode().getWidth()-1024)/2, (device.getDisplayMode().getHeight()-768)/2);
-		}	
-		
+			setLocation((device.getDisplayMode().getWidth() - 1024) / 2,
+					(device.getDisplayMode().getHeight() - 768) / 2);
+		}
+
 		// Hide the bar at the top of the window and the window border
 		this.setUndecorated(true);
-		
+
 		// Add the JLayeredPane to our contents
 		displayPane_ = new DisplayDesktopPane(this.getSize());
 		getContentPane().add(displayPane_);
-		
+
 		// Add the key listener
 		this.addKeyListener(JKeyListener.getInstance());
-		
+
+		// Add state listener
+		this.addWindowListener(this);
+
 		// Show the window
 		setVisible(true);
-		
+
 		// Make the window fullscreen
-		if ((FULL_SCREEN) && (!fakeFullscreen)) device.setFullScreenWindow(this);
+		if ((FULL_SCREEN) && (!fakeFullscreen))
+			device.setFullScreenWindow(this);
 	}
 
 	public DisplayDesktopPane getDisplayDesktopPane() {
 		return displayPane_;
+	}
+
+	/*
+	 * Window State Listeners
+	 */
+
+	/*
+	 * Called when the game window is minimized
+	 */
+	public void windowIconified(WindowEvent e) {
+		// Restore the game window to full screen
+		// Workaround for bug with JVM 1.6.0_12-b04 that
+		// auto minimizes for JOptionPane method calls
+		this.setState(NORMAL);
+	}
+
+	public void windowActivated(WindowEvent e) {
+	}
+
+	public void windowClosed(WindowEvent e) {
+	}
+
+	public void windowClosing(WindowEvent e) {
+	}
+
+	public void windowDeactivated(WindowEvent e) {
+	}
+
+	public void windowDeiconified(WindowEvent e) {
+	}
+
+	public void windowOpened(WindowEvent e) {
 	}
 }
