2f66e6779afac1b9ea38f5f48a76ee4f34edc555
bengotow <bengotow@21451752-e034-11dd-bd7b-b12b6590754f>
2009-03-22 23:14:42 +0000

    - Added ability for tiles to have meeple. - Added Meeple class to the org.javassone.model package - Meeple constructor takes lots of parameters, but should give us enough information to create a meeple sprite on the map from the meeple model object. - Can test for a meeple at a specific region on a tile using Tile.meepleInRegion(), Tile.meepleInQuadrant()

---
+++ javassonne/src/org/javassonne/model/Meeple.java
@@ -0,0 +1,95 @@
+/**
+ * Javassonne 
+ *  http://code.google.com/p/javassonne/
+ * 
+ * @author [Add Name Here]
+ * @date Mar 22, 2009
+ * 
+ * Copyright 2009 Javassonne Team
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License. 
+ *  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software 
+ *  distributed under the License is distributed on an "AS IS" BASIS, 
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ *  implied. See the License for the specific language governing 
+ *  permissions and limitations under the License. 
+ */
+
+package org.javassonne.model;
+
+import java.awt.Point;
+
+public class Meeple {
+
+	private Tile parentTile_;
+	private Point parentTileLocation_;
+
+	private Tile.Quadrant quadrantOnTile_;
+	private Tile.Region regionOnTile_;
+
+	private int player_;
+
+	/**
+	 * The default constructor for a meeple. It takes a lot of parameters, but
+	 * this information is needed for optimization later.
+	 * 
+	 * @param player The number of the player whose meeple this is
+	 * @param tile The tile the meeple has been placed on
+	 * @param tileLocation The location of the tile (so we can draw fast)
+	 * @param quadrant The quadrant the meeple is on, if applicable.
+	 * @param region The region the meeple is on, if applicable. 
+	 */
+	public Meeple(int player, Tile tile, Point tileLocation,
+			Tile.Quadrant q, Tile.Region r) {
+
+		parentTileLocation_ = tileLocation;
+		parentTile_ = tile;
+		player_ = player;
+		
+		quadrantOnTile_ = q;
+		regionOnTile_ = r;
+		
+	}
+
+	public Tile getParentTile() {
+		return parentTile_;
+	}
+
+	public Point getParentTileLocation() {
+		return parentTileLocation_;
+	}
+
+	public int getPlayer() {
+		return player_;
+	}
+	
+	public Tile.Region getRegionOnTile() {
+		return regionOnTile_;
+	}
+
+	public Tile.Quadrant getQuadrantOnTile() {
+		return quadrantOnTile_;
+	}
+	
+	public void setParentTile(Tile parentTile) {
+		this.parentTile_ = parentTile;
+	}
+
+	public void setParentTileLocation(Point parentTileLocation) {
+		this.parentTileLocation_ = parentTileLocation;
+	}
+
+	public void setPlayer(int player) {
+		this.player_ = player;
+	}
+	
+	public void setRegionOnTile(Tile.Region region) {
+		this.regionOnTile_ = region;
+	}
+	
+	public void setQuadrantOnTile(Tile.Quadrant quadrant) {
+		this.quadrantOnTile_ = quadrant;
+	}
+
+}
