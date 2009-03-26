8edd3bfffbc15d548cd5ea570a0ca4081e68611e
bengotow <bengotow@21451752-e034-11dd-bd7b-b12b6590754f>
2009-03-26 03:37:41 +0000

    - You can type more characters into the chat now - Chat is displayed in white instead of black

--- javassonne/src/org/javassonne/networking/ChatManager.java
+++ javassonne/src/org/javassonne/networking/ChatManager.java
@@ -46,16 +46,7 @@ public class ChatManager implements Iterable<String> {
 	private Mode mode_;
 
 	// Currently contains only alphanumeric, should prob add symbols
-	private static int[] allowedKeys_ = { KeyEvent.VK_A, KeyEvent.VK_B,
-			KeyEvent.VK_C, KeyEvent.VK_D, KeyEvent.VK_E, KeyEvent.VK_F,
-			KeyEvent.VK_G, KeyEvent.VK_H, KeyEvent.VK_I, KeyEvent.VK_J,
-			KeyEvent.VK_K, KeyEvent.VK_L, KeyEvent.VK_M, KeyEvent.VK_N,
-			KeyEvent.VK_O, KeyEvent.VK_P, KeyEvent.VK_Q, KeyEvent.VK_R,
-			KeyEvent.VK_S, KeyEvent.VK_T, KeyEvent.VK_U, KeyEvent.VK_V,
-			KeyEvent.VK_W, KeyEvent.VK_X, KeyEvent.VK_Y, KeyEvent.VK_Z,
-			KeyEvent.VK_0, KeyEvent.VK_1, KeyEvent.VK_2, KeyEvent.VK_3,
-			KeyEvent.VK_4, KeyEvent.VK_5, KeyEvent.VK_6, KeyEvent.VK_7,
-			KeyEvent.VK_8, KeyEvent.VK_9, KeyEvent.VK_SPACE };
+	private static String allowed_ = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()[]{}<>,.?/\\|;':\"~`- ";
 
 	private ChatManager() {
 		currentMessage_ = new StringBuffer();
@@ -102,11 +93,13 @@ public class ChatManager implements Iterable<String> {
 			privateMessages_.remove(NUMBER_PRIVATE_MESSAGES);
 	}
 
-	public static void KeyReleased(KeyEvent e) {
-		getInstance()._KeyReleased(e);
+	public static void KeyPressed(KeyEvent e) {
+		getInstance()._KeyPressed(e);
 	}
 
-	private void _KeyReleased(KeyEvent e) {
+	private void _KeyPressed(KeyEvent e) {
+		String s = String.valueOf(e.getKeyChar());
+		
 		// Detect enter key
 		if (e.getKeyCode() == KeyEvent.VK_ENTER)
 			handleEnter();
@@ -116,8 +109,8 @@ public class ChatManager implements Iterable<String> {
 			handleBackspace();
 
 		// Detect other valid key
-		else if (validKey(e.getKeyCode())) {
-			currentMessage_.append(e.getKeyChar());
+		else if (allowed_.contains(s.toLowerCase())) {
+			currentMessage_.append(s);
 			updateCurrentMessage();
 		}
 	}
@@ -178,13 +171,6 @@ public class ChatManager implements Iterable<String> {
 					Notification.SEND_PRIVATE_CHAT, cm);
 
 		}
-	}
-
-	private boolean validKey(int keyCode) {
-		for (int i = 0; i < allowedKeys_.length; i++)
-			if (allowedKeys_[i] == keyCode)
-				return true;
-		return false;
 	}
 
 	public static Iterator<String> getIterator() {
