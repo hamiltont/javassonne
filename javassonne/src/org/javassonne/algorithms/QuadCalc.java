9d3c5c506c15ec852198265cda8efd5e2a0439a3
corespace <corespace@21451752-e034-11dd-bd7b-b12b6590754f>
2009-04-14 19:33:20 +0000

    - Commented Calculators and fixed edge case of monasteries in RegionsCalc

--- javassonne/src/org/javassonne/algorithms/QuadCalc.java
+++ javassonne/src/org/javassonne/algorithms/QuadCalc.java
@@ -32,45 +32,83 @@ import org.javassonne.model.TileBoardIterator;
 import org.javassonne.model.Tile.Quadrant;
 import org.javassonne.ui.GameState;
 
+/**
+ * Calculates Quadrant scores
+ * 
+ * A QuadCalc should be thrown away if Tiles are being added to board because it
+ * does not update cached data
+ * 
+ * TODO: Enforce this by listening for board_changed events?
+ */
 public class QuadCalc {
 
 	public QuadCalc() {
-
+		// Initialize data structures
 		numCastles_ = new HashMap<Point, EnumMap<Tile.Quadrant, Integer>>();
 		globalMeep_ = new HashMap<Point, EnumMap<Tile.Quadrant, List<Meeple>>>();
-
 	}
 
+	/*
+	 * External function for calculating farm values for given quadrant and
+	 * connecting quadrants. Delegates most of calculation to private recursive
+	 * function.
+	 */
 	public void traverseQuadrant(TileBoardIterator iter, Tile.Quadrant quad) {
+		// Initialize recursion variables for compounding list of
+		// tile-quadrants, etc.
 		HashMap<Point, ArrayList<Tile.Quadrant>> list = new HashMap<Point, ArrayList<Tile.Quadrant>>();
 		ArrayList<Meeple> meeps = new ArrayList<Meeple>();
+		// Call recursive function
 		traverseQuadrant(iter, quad, meeps, list);
-		// TODO: Get total as # completed castles
+
 		int total = 0;
+		// This is ugly, but we have to calculate how many contiguous regions
+		// are connected to this quadrant area.
 		RegionsCalc c = new RegionsCalc();
+
+		// For each Tile in our Quadrant area
 		for (Point p : list.keySet()) {
+			// Make an iterator to it
 			TileBoardIterator iterNew = new TileBoardGenIterator(GameState
 					.getInstance().getBoard(), p);
+			// For each quadrant on this Tile in our Quadrant area
 			for (Tile.Quadrant q : list.get(p)) {
+
 				for (Tile.Region r : Tile.Region.values()) {
+					/*
+					 * If the region is not null and it would get points from a
+					 * farm and we have not yet visited it with our RegionsCalc
+					 * (to stop double-counting of regions) and it is adjacent
+					 * to our current quadrant, traverse it in the RegionsCalc
+					 * ...
+					 */
 					if (iterNew.current().featureInRegion(r) != null
 							&& iterNew.current().featureInRegion(r).farmPointValue != 0
 							&& c.getScoreOfRegion(p, r) == -1
 							&& r.isAdjacentTo(q)) {
 						c.traverseRegion(iterNew, r);
+						// ... and add its points if it is complete
 						if (c.getRegionCompletion(p, r))
 							total += iterNew.current().featureInRegion(r).farmPointValue;
 					}
 				}
 			}
 		}
+
+		/*
+		 * Now we update our global data structures This update will do nothing
+		 * if we've already touched the entrance quadrant because the recursive
+		 * function will have returned without modifying list or meeps
+		 */
 		for (Point p : list.keySet()) {
+			// Make sure we don't try to put in a null data structure
 			if (numCastles_.get(p) == null)
 				numCastles_.put(p, new EnumMap<Tile.Quadrant, Integer>(
 						Tile.Quadrant.class));
 			if (globalMeep_.get(p) == null)
 				globalMeep_.put(p, new EnumMap<Tile.Quadrant, List<Meeple>>(
 						Tile.Quadrant.class));
+
 			for (Tile.Quadrant q : list.get(p)) {
 				numCastles_.get(p).put(q, total);
 				globalMeep_.get(p).put(q, meeps);
@@ -80,30 +118,40 @@ public class QuadCalc {
 
 	}
 
+	// Private recursive function for calculating Quadrant values
 	private void traverseQuadrant(TileBoardIterator iter, Quadrant quad,
 			ArrayList<Meeple> meeps, HashMap<Point, ArrayList<Quadrant>> list) {
+		// Base case 1: off the edge of the board
 		if (iter.current() == null)
 			return;
+		// Base case 2: we've already touched this quadrant
 		if (getNumCastles(iter.getLocation(), quad) != -1)
 			return;
 
 		// else
+
+		// "touch" this quadrant globally...
 		if (numCastles_.get(iter.getLocation()) == null)
 			numCastles_.put(iter.getLocation(),
 					new EnumMap<Tile.Quadrant, Integer>(Tile.Quadrant.class));
 		numCastles_.get(iter.getLocation()).put(quad, 0);
+		// ... and locally
 		if (list.get(iter.getLocation()) == null)
 			list.put(iter.getLocation(), new ArrayList<Tile.Quadrant>());
 		list.get(iter.getLocation()).add(quad);
+
+		// If there's a merson here, add it to our local list
 		Meeple current = iter.current().meepleInQuadrant(quad);
 		if (current != null)
 			meeps.add(current);
 
+		// Temp store walls on the sides of this Tile - see Tile docs for more
+		// info
 		boolean leftWall = iter.current().farmWallInRegion(Tile.Region.Left);
 		boolean upWall = iter.current().farmWallInRegion(Tile.Region.Top);
 		boolean rightWall = iter.current().farmWallInRegion(Tile.Region.Right);
 		boolean downWall = iter.current().farmWallInRegion(Tile.Region.Bottom);
-		// traverse to next Tile(s)
+		// traverse to next Tile(s) from edges of current quadrant
 		if (quad.equals(Tile.Quadrant.TopLeft)) {
 			if (!leftWall)
 				traverseQuadrant(((TileBoardGenIterator) iter).leftCopy(),
@@ -133,7 +181,9 @@ public class QuadCalc {
 				traverseQuadrant(((TileBoardGenIterator) iter).downCopy(),
 						Tile.Quadrant.TopRight, meeps, list);
 		}
-		// traverse to other quadrants that connect to current
+
+		// traverse to other quadrants on this Tile that connect to the current
+		// (See Tile doc for info on quadrant connectivity)
 		int currentQuad = iter.current().farmInQuadrant(quad);
 		for (Tile.Quadrant q : Tile.Quadrant.values()) {
 			if (iter.current().farmInQuadrant(q) == currentQuad)
@@ -143,8 +193,11 @@ public class QuadCalc {
 		return;
 	}
 
-	// If traverseQuadrant has touched given Quadrant of Tile at given location
-	// This function returns the size of the Quadrant, else, returns -1
+	/*
+	 * If traverseQuadrant has touched given Quadrant of Tile at given location,
+	 * this function returns the num of regions "fed" by the Quadrant area,
+	 * else, returns -1
+	 */
 	public Integer getNumCastles(Point loc, Tile.Quadrant quad) {
 		Map<Tile.Quadrant, Integer> tileQuadrants = numCastles_.get(loc);
 		if (tileQuadrants == null)
@@ -157,6 +210,11 @@ public class QuadCalc {
 		return temp;
 	}
 
+	/*
+	 * If traverseQuadrant has touched given Quadrant of Tile at given location
+	 * and it has a nonempty meeple list claiming it, this function returns the
+	 * list of meeple, else returns empty list
+	 */
 	public List<Meeple> getMeepleList(Point loc, Tile.Quadrant quad) {
 		ArrayList<Meeple> returnVal = new ArrayList<Meeple>();
 		Map<Tile.Quadrant, List<Meeple>> tileQuadrants = globalMeep_.get(loc);
@@ -171,7 +229,15 @@ public class QuadCalc {
 		return returnVal;
 	}
 
-	// Keeps track of touched locations
+	/*
+	 * Keeps track of touched locations. These store data collected when
+	 * traversing and make it available to the accessors. Also, the recursive
+	 * function can quit if we've already traversed this quadrant with this
+	 * calculator - this saves time if the function is accidentally called twice
+	 * (i.e. easier code to traverse all), but causes problems if the data is
+	 * dirty. Therefore, a QuadCalc should be thrown away if Tiles are being
+	 * added to board.
+	 */
 	private HashMap<Point, EnumMap<Tile.Quadrant, Integer>> numCastles_;
 	private HashMap<Point, EnumMap<Tile.Quadrant, List<Meeple>>> globalMeep_;
 
