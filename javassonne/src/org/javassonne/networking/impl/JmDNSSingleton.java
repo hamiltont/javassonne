5de1a4a6268050185cd92d87fbb1886dcbda3dd1
hamiltont <hamiltont@21451752-e034-11dd-bd7b-b12b6590754f>
2009-04-16 16:45:06 +0000

    -- Added a way to inspect all calls to jmdns and where they came from in real time, and a DEBUG flag. Output is only of interest for network debugging, so set to false so no one else get's bombarded ;)

--- javassonne/src/org/javassonne/networking/impl/JmDNSSingleton.java
+++ javassonne/src/org/javassonne/networking/impl/JmDNSSingleton.java
@@ -23,12 +23,18 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceInfo;
+import javax.jmdns.ServiceListener;
+import javax.jmdns.ServiceTypeListener;
 
 import org.javassonne.logger.LogSender;
 
 public class JmDNSSingleton {
 
 	private static JmDNS jmdns_ = null;
+	private static jmdnsCallThru callThru_ = null;
+
+	private static boolean DEBUG = false;
 
 	private JmDNSSingleton() {
 	}
@@ -53,7 +59,222 @@ public class JmDNSSingleton {
 				e.printStackTrace();
 			}
 		}
+		callThru_ = new jmdnsCallThru();
+		return callThru_;
+	}
 
-		return jmdns_;
+	private static class jmdnsCallThru extends JmDNS {
+
+		public void addServiceListener(String arg0, ServiceListener arg1) {
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " accessing addServiceListener");
+			jmdns_.addServiceListener(arg0, arg1);
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " leaving addServiceListener");
+		}
+
+		public void addServiceTypeListener(ServiceTypeListener arg0)
+				throws IOException {
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " accessing addServiceTypeListener");
+			jmdns_.addServiceTypeListener(arg0);
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " leaving addServiceTypeListener");
+		}
+
+		public void close() {
+			if (DEBUG)
+				LogSender
+						.sendInfo("JmDNS CallThru - Thread "
+								+ Thread.currentThread().getName()
+								+ " accessing close");
+			jmdns_.close();
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName() + " leaving close");
+		}
+
+		public String getHostName() {
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " accessing getHostName");
+			String s = jmdns_.getHostName();
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " leaving getHostName");
+			return s;
+		}
+
+		public InetAddress getInterface() throws IOException {
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " accessing getInterface");
+
+			InetAddress a = jmdns_.getInterface();
+
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " leaving getInterface");
+			return a;
+		}
+
+		public ServiceInfo getServiceInfo(String arg0, String arg1) {
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " accessing getServiceInfo");
+			ServiceInfo a = jmdns_.getServiceInfo(arg0, arg1);
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " leaving getServiceInfo");
+			return a;
+		}
+
+		public ServiceInfo getServiceInfo(String arg0, String arg1, int arg2) {
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " accessing getServiceInfo (with timout)");
+			ServiceInfo a = jmdns_.getServiceInfo(arg0, arg1, arg2);
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " leaving getServiceInfo (with timeout)");
+			return a;
+		}
+
+		public ServiceInfo[] list(String arg0) {
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName() + " accessing list");
+			ServiceInfo[] a = jmdns_.list(arg0);
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName() + " leaving list");
+			return a;
+		}
+
+		public void printServices() {
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " accessing printServices");
+			jmdns_.printServices();
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " leaving printServices");
+		}
+
+		public void registerService(ServiceInfo arg0) throws IOException {
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " accessing registerService");
+			jmdns_.registerService(arg0);
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " leaving registerService");
+		}
+
+		public void registerServiceType(String arg0) {
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " accessing registerServiceType");
+			jmdns_.registerServiceType(arg0);
+
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " leaving registerServiceType");
+		}
+
+		public void removeServiceListener(String arg0, ServiceListener arg1) {
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " accessing removeServiceListener");
+			jmdns_.removeServiceListener(arg0, arg1);
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " leaving removeServiceListener");
+		}
+
+		public void removeServiceTypeListener(ServiceTypeListener arg0) {
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " accessing removeServiceTypeListener");
+			jmdns_.removeServiceTypeListener(arg0);
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " leaving removeServiceTypeListener");
+		}
+
+		public void requestServiceInfo(String arg0, String arg1) {
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " accessing requestServiceInfo");
+			jmdns_.requestServiceInfo(arg0, arg1);
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " leaving requestServiceInfo");
+		}
+
+		public void requestServiceInfo(String arg0, String arg1, int arg2) {
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " accessing requestServiceInfo (with timeout)");
+			jmdns_.requestServiceInfo(arg0, arg1, arg2);
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " leaving requestServiceInfo (with timeout)");
+		}
+
+		public void unregisterAllServices() {
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " accessing unregisterAllServices");
+			jmdns_.unregisterAllServices();
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " leaving unregisterAllServices");
+		}
+
+		public void unregisterService(ServiceInfo arg0) {
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " accessing unregisterService");
+			jmdns_.unregisterService(arg0);
+			if (DEBUG)
+				LogSender.sendInfo("JmDNS CallThru - Thread "
+						+ Thread.currentThread().getName()
+						+ " leaving unregisterService");
+		}
+
 	}
 }
